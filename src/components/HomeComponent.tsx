f2a011c668275d78be3bafd9a53843611218cdc0
umair <uk962571@gmail.com>
2023-07-25 16:02:23 +0530

    first commit

---
+++ src/components/HomeComponent.tsx
@@ -0,0 +1,18 @@
+
+
+
+export function HomeComponent(){
+
+    
+    return(
+        <div >
+<select >
+    <option >Gender</option>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="other">other</option>
+</select>
+
+        </div>
+    );
+}
